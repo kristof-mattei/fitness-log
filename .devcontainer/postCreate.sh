8cb5b7d8ead8cced9fb22c038ad0893f6e581875
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-08-25 19:56:52 -0700

    chore: switching between dev container and host made pnpm exit with `ERR_PNPM_UNEXPECTED_STORE`

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -8,3 +8,8 @@ rustup component add --toolchain nightly rustfmt
 cargo install sqlx-cli --no-default-features --features postgres
 
 cargo sqlx migrate run --source ./crates/fitness-log/migrations
+
+# the named volume mounts as root, pnpm runs as the remote user
+sudo chown "$(id --user):$(id --group)" node_modules
+
+pnpm install
